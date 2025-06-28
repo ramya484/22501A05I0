fdd24907b2d838b75307e6c6d1f624bd0faddb90
ramya <ramyatirandi484>
2025-06-28 10:59:28 +0530

    Initial

---
+++ url_short/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <h2> URL SHORTENER  </h2>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
